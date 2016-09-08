39e6a8955b3119b8c8226bffda2b890d457c4786
sidhuaman <amandeep.sidhu@my.jcu.edu.au>
2016-09-09 07:00:36 +1000

    error checking in mark_item function,required_item function and complete_item function

--- name.py
+++ name.py
@@ -1,3 +1,6 @@
-name = input("Enter your name:")
-for char in name:
- print(name)
+name = input("Please enter your name : ")
+print("Every second letter of the name " + name + " is ", end="")
+
+for i in range(len(name)):
+    if i % 2 == 1:
+        print(name[i] + " ", end="")
